aa47642fffa8d979b486a140b52ea1286fd27108
Marcus-RR <145934289+Marcus-RR@users.noreply.github.com>
2023-12-13 23:41:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 #include <cctype>
 #include "Cipher.h"
+#include <locale>
 using namespace std;
-void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+void check(const wstring& Text, const int & key)
 {
+    wstring s=Text;
     try {
-        wstring cipherText;
-        wstring decryptedText;
-        Cipher cipher(key);
-        cipherText = cipher.encrypt(Text);
-        if (destructCipherText)
-            cipherText.front() = tolower(cipherText.front());
-        decryptedText = cipher.decrypt(cipherText);
-        wcout<<"key="<<key<<endl;
-        wcout<<Text<<endl;
-        wcout<<cipherText<<endl;
-        wcout<<decryptedText<<endl;
+        Cipher kluch(key);
+        wcout<<kluch.zakodirovat(key, s)<<endl;
+        wstring g=kluch.zakodirovat(key, s);
+        wcout<<kluch.raskodirovar(key, g)<<endl;
     } catch (const cipher_error & e) {
         cerr<<"Error: "<<e.what()<<endl;
     }
 }
-int main(int argc, char **argv)
+int main()
 {
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
     wcout<<L'-'<<endl;
-    check(L"ТЕКСТДЛЯПРОВЕРКИ",L"ПРИВЕТ");
+    check(L"",10);
     wcout<<L'-'<<endl;
-    check(L"ТЕКСТДЛЯПРОВЕРКИ",L"");
+    check(L"TEXTTOCHECK",10);
     wcout<<L'-'<<endl;
-    check(L"ТЕКСТДЛЯПРОВЕРКИ",L"П1Р2И3В4Е5Т6");
+    check(L"TEXTTOCHECK",1);
     wcout<<L'-'<<endl;
-    check(L"ТЕКСТ ДЛЯ ПРОВЕРКИ",L"ПРИВЕТ");
+    check(L"TEXTTOCHECK",0);
     wcout<<L'-'<<endl;
-    check(L"ТЕКСТДЛЯПРОВЕРКИ",L"ПРИВЕТ",true);
-    wcout<<L'-'<<endl;
-    check(L"21908321786912367801327860",L"ПРИВЕТ");
+    check(L"TEXTTOCHECK",3);
     wcout<<L'-'<<endl;
     return 0;
 }
